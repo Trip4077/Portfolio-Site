fad03f5a2805778ea20c1a157176151f5c96ea94
Trip1701 <bj2336@email.vccs.edu>
2018-11-21 16:32:04 -0500

    SKills Icon Animation v1.0 implemented

--- script.js
+++ script.js
@@ -5,15 +5,17 @@ const navLinks = document.querySelectorAll('.nav__link a');
 
 
 //Event listeners for Link Display Settings
+//Link text shows on mouse enter
 navLinks.forEach(link => link.addEventListener('mouseenter', function(event) {
   return event.target.classList.add('active_hover');
 }));
 
-
+//Link text hidden on mouse leave
 navLinks.forEach(link => link.addEventListener('mouseleave', function(event) {
   return event.target.classList.remove('active_hover');
 }));
 
+//Navigation changes active link on click
 navLinks.forEach(link => link.addEventListener('click', function(event) {
   navLinks.forEach(link => link.classList.remove('active'));
 
@@ -59,4 +61,66 @@ arrow.addEventListener('click', function() {
     navLinks[count].classList.add('active');
   }
 })
-console.log(arrow)
+
+//****************Skills****************
+//Array of skill descriptions
+const skills = {
+  html: ["HTML5", "Semantic HTML", "Presentational HTML"],
+  css: ["CSS3", "Animations", "Specificity", "BEM Methodology"],
+  js: ["ES5", "ES6", "Closures", "Callbacks", "AJAX", "DOM"],
+  less: ["Preprocessor", "Nested Styles", "Mixins", "Variables"],
+  bootstrap: ["Bootstrap4 library", "Components"],
+  gsap: ["GSAP Library", "JS Animations"],
+  react: ["ReactJS", "Front-end library", "Functional Components"]
+}
+
+class SkillBox {
+  constructor(element) {
+
+    this.element = element;
+    this.title = this.element.dataset.tab;
+    this.img = this.element.dataset.path;
+    this.description = skills[this.title];
+
+    this.element.addEventListener('mouseenter', () => { this.displayText() });
+    this.element.addEventListener('mouseleave', () => { this.hideText() });
+  }
+
+  displayText() {
+
+    if(this.element.children.length < 2) {
+
+      this.element.removeChild(this.element.children[0]);
+
+      this.description.forEach(content => {
+
+        const element = document.createElement('p');
+        element.innerText = content;
+        element.style.color = '#FE3301';
+
+        this.element.appendChild(element);
+
+      });
+    }
+
+    TweenMax.fromTo(this.element, 1.5, {css: {opacity: 0}}, {css:{opacity: 1}});
+  }
+
+  hideText() {
+
+    for(let i = 0; i < this.element.children.length; i++) {
+      this.element.removeChild(this.element.children[i]);
+      i--;
+    }
+
+    const img = document.createElement('img');
+    img.src = this.img;
+    img.id = this.title;
+    this.element.appendChild(img)
+
+    TweenMax.fromTo(this.element, 2.25, {css: {opacity: 0}}, {css:{opacity: 1}});
+  }
+}
+
+skillIcons = document.querySelectorAll('.skills__row__items.front-end div')
+                  .forEach(elem => new SkillBox(elem))
